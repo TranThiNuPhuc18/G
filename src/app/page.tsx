ddc001d8972a87991cdaf21bdd611db2db24f024
TranThiNuPhuc18 <tranthinuphuc.bd@gmail.com>
2023-12-15 03:42:02 +0700

    loi hinh anh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Image from 'next/image'
 import React from 'react';
-import '../styles/app.css'
+import '../styles/app.css';
+import img1 from '../img/01.jpg';
 
-export default function Page()
+export function Page()
 {
-  // return <h1>Hello</h1>
   return (
-        // <header class="masthead text-center text-white">
-        <>        
+    <>        
+    <head>       
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>One Page Wonder - Start Bootstrap Template</title> 
+    </head>
+    <body id="page-top">
         <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
             <div className="container px-5">
                 <a className="navbar-brand" href="#page-top">GARA TRUNG HẬU</a>
@@ -32,16 +36,74 @@ export default function Page()
             <div className="bg-circle-3 bg-circle"></div>
             <div className="bg-circle-4 bg-circle"></div>
         </header>
-
+        <section id="scroll">
+            <div className="container px-5">
+                <div className="row gx-5 align-items-center">
+                    <div className="col-lg-6 order-lg-2">
+                        <div className="p-5"><img className="img-fluid rounded-circle" src="assets/img/anim.png" alt="..." /></div>
+                    </div>
+                    <div className="col-lg-6 order-lg-1">
+                        <div className="p-5">
+                            <h2 className="display-4">For those about to rock...</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className="container px-5">
+                <div className="row gx-5 align-items-center">
+                    <div className="col-lg-6">
+                        <div className="p-5"><img className="img-fluid rounded-circle" src="../img/01.jpg" alt="..." /></div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="p-5">
+                            <h2 className="display-4">We salute you!</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className="container px-5">
+                <div className="row gx-5 align-items-center">
+                    <div className="col-lg-6 order-lg-2">
+                        <div className="p-5"><img className="img-fluid rounded-circle" src="assets/img/M3.png" alt="..." /></div>
+                    </div>
+                    <div className="col-lg-6 order-lg-1">
+                        <div className="p-5">
+                            <h2 className="display-4">Let there be rock!</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer className="py-5 bg-black">
+            <div className="container px-5"><p className="m-0 text-center text-white small">Copyright &copy; Your Website 2023</p></div>
+        </footer>
+        
+        {/* Bootstrap core JS */}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+      
+        {/* Core theme JS */}
+        <script src="js/scripts.js"></script>
+        </body>
         </>
 
      
      
-     // <div >
-            //     <div>
-            //         <h1> One Page Wonder</h1>
-            //         <h2>Will Rock Your Socks Off</h2>
-            //     </div>
-            // </div>
+     
   );
 } 
+
+
+function MyComponent() {
+    return (
+      <img className="img-fluid rounded-circle" src={img1} alt="..." />
+    );
+  }
+  
+  export default Page;
