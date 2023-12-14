33868ec03572b5400b1b7379bae0cdb206f556d4
TranThiNuPhuc18 <tranthinuphuc.bd@gmail.com>
2023-12-14 17:11:15 +0700

    fix nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,25 @@ export default function Page()
   // return <h1>Hello</h1>
   return (
         // <header class="masthead text-center text-white">
-            
-    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-        <div class="container px-5">
-            <a class="navbar-brand" href="#page-top">GARA TRUNG HẬU</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="service.html">DỊCH VỤ</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#!">LIÊN HỆ</a></li>
-                </ul>
+        <>        
+        <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+            <div className="container px-5">
+                <a className="navbar-brand" href="#page-top">GARA TRUNG HẬU</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item"><a className="nav-link" href="service.html">DỊCH VỤ</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#!">LIÊN HỆ</a></li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-            // <div >
+        </nav>
+        
+        </>
+
+     
+     
+     // <div >
             //     <div>
             //         <h1> One Page Wonder</h1>
             //         <h2>Will Rock Your Socks Off</h2>
