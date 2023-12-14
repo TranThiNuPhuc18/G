91ba2baf0a56d8ac14afd9c0500a0e4249db9681
TranThiNuPhuc18 <tranthinuphuc.bd@gmail.com>
2023-12-14 17:13:00 +0700

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,19 @@ export default function Page()
                 </div>
             </div>
         </nav>
-        
+        <header className="masthead text-center text-white">
+            <div className="masthead-content">
+                <div className="container px-5">
+                    <h1 className="masthead-heading mb-0">One Page Wonder</h1>
+                    <h2 className="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
+                </div>
+            </div>
+            <div className="bg-circle-1 bg-circle"></div>
+            <div className="bg-circle-2 bg-circle"></div>
+            <div className="bg-circle-3 bg-circle"></div>
+            <div className="bg-circle-4 bg-circle"></div>
+        </header>
+
         </>
 
      
