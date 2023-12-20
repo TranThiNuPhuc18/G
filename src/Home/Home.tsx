5c7da3aad77ad41abd577c75c3ff4da01f59a190
TranThiNuPhuc18 <tranthinuphuc.bd@gmail.com>
2023-12-20 14:34:49 +0700

    create foder home, post, service

---
+++ src/Home/Home.tsx
@@ -0,0 +1,108 @@
+import Image from 'next/image';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import '../styles/app.css';
+import img4 from '../img/04.png';
+import img5 from '../img/005.png';
+import img6 from '../img/06.png';
+import NavComponent from '../components/nav/nav.component';
+// Add this line to import Bootstrap CSS from a CDN
+// import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
+// import "../styles/app.css";
+import "../styles/home.css"
+export function Home()
+{
+  return (
+    <>        
+    <head>       
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>One Page Wonder - Start Bootstrap Template</title> 
+    </head>
+    <body id="page-top">
+       <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+            <div className="container px-5">
+                <a className="navbar-brand" href="#page-top">GARA TRUNG HẬU</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">DỊCH VỤ</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">LIÊN HỆ</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav> 
+        <header className="masthead text-center text-white">
+            <div className="masthead-content">
+                <div className="container px-5">
+                    <h1 className="masthead-heading mb-0">One Page Wonder</h1>
+                    <h2 className="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
+                </div>
+            </div>
+            <div className="bg-circle-1 bg-circle"></div>
+            <div className="bg-circle-2 bg-circle"></div>
+            <div className="bg-circle-3 bg-circle"></div>
+            <div className="bg-circle-4 bg-circle"></div>
+        </header>
+        <section id="scroll">
+            <div className="container px-5">
+                <div className="row gx-5 align-items-center">
+                    <div className="col-lg-6 order-lg-2">
+                        <div className="p-5"><img className="img-fluid rounded-circle" src={img4.src} alt="..." /></div>
+                    </div>
+                    <div className="col-lg-6 order-lg-1">
+                        <div className="p-5">
+                            <h2 className="display-4">For those about to rock...</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className="container px-5">
+                <div className="row gx-5 align-items-center">
+                    <div className="col-lg-6">
+                        <div className="p-5"><img className="img-fluid rounded-circle" src={img5.src} alt="..." /></div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="p-5">
+                            <h2 className="display-4">We salute you!</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className="container px-5">
+                <div className="row gx-5 align-items-center">
+                    <div className="col-lg-6 order-lg-2">
+                        <div className="p-5"><img className="img-fluid rounded-circle" src={img6.src} alt="..." /></div>
+                    </div>
+                    <div className="col-lg-6 order-lg-1">
+                        <div className="p-5">
+                            <h2 className="display-4">Let there be rock!</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer className="py-5 bg-black">
+            <div className="container px-5"><p className="m-0 text-center text-white small">Copyright &copy; Your Website 2023</p></div>
+        </footer>
+        
+        {/* Bootstrap core JS */}
+        {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> */}
+        </body>
+        </>
+
+
+  );
+} 
+export default Home;
